e8b0c3c0c213ad8b0603223fc5877d003977e49b
Prajwal Patil <patilprajwal264@gmail.com>
2024-03-13 22:47:51 +0530

    Add project files.

---
+++ BookMyMeal/MealDbContext/MealDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace BookMyMeal.MealDbContext
+{
+    public class MealDbContext:DbContext
+    {
+        public MealDbContext(DbContextOptions<MealDbContext> options):base(options) { 
+        
+        
+        }
+
+    }
+}
---
+++ BookMyMeal/Model/Coupon.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace BookMyMeal.Model
+{
+ 
+    public class CouponDetails
+    {
+        public DateTime Date { get; set; }
+        public string Day {  get; set; }    
+        public string EmployeeName { get; set; }
+        public int EmployeeId { get; set; }
+        public string CouponNo { get; set; }
+        public string RedemptionStatus { get; set; }
+        public string MealType { get; set; }
+    }
+    
+}
